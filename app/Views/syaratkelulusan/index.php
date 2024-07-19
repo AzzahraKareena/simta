719b41f5629e0d1e45f824d4e57e4eadd591f59a
Revydo Susilo <revydo2x@gmail.com>
2024-07-19 22:32:44 +0700

    Revisi sub untuk bagian sidang

--- app/Views/syaratkelulusan/index.php
+++ app/Views/syaratkelulusan/index.php
@@ -39,9 +39,9 @@
         <div class="d-flex overflow-auto h-55px">
             <ul class="nav nav-stretch nav-line-tabs nav-line-tabs-2x border-transparent fs-6 fw-bold flex-nowrap">
                 <!--begin::Nav item-->
-                <li class="nav-item">
+                <!-- <li class="nav-item">
                     <a class="nav-link text-active-primary me-6" href="<?= base_url('pengajuanbimbingan') ?>">Pengajuan Bimbingan</a>
-                </li>
+                </li> -->
                 <!--end::Nav item-->
                 <!--begin::Nav item-->
                 <li class="nav-item">
