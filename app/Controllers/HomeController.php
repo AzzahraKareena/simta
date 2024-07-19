3d193a17d4621556081615abae23eca68f1cf535
Revydo Susilo <revydo2x@gmail.com>
2024-07-19 11:27:26 +0700

    notifikasi mahasiswa bimbingan

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,16 +3,17 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\MahasiswaBimbinganModel;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class HomeController extends BaseController
 {
-    public function index()
-    {
-        $operation['title'] = 'Home';
-        $operation['sub_title'] = 'Sistem Informasi Manajemen Tugas Akhir';
-        return view("dashboard/index", $operation);
-    }
+    // public function index()
+    // {
+    //     $operation['title'] = 'Home';
+    //     $operation['sub_title'] = 'Sistem Informasi Manajemen Tugas Akhir';
+    //     return view("dashboard/index", $operation);
+    // }
     // public function dashboard()
     // {
     //     $operation['title'] = 'Dashboard';
@@ -20,4 +21,28 @@ class HomeController extends BaseController
     //     return view("dashboard/index", $operation);
     // }
 
+    public function index()
+    {
+        $model = new MahasiswaBimbinganModel();
+        $dosenId = session()->get('user_id');
+        $tahun = $this->request->getVar('tahun') ?? date('Y');
+        $data = $model->getUserByDosen($dosenId, $tahun);
+        
+        // Debugging data
+        error_log(print_r($data, true));
+
+        if ($data) {
+            foreach ($data as $item) {
+                $dataString = '<b>' . htmlspecialchars($item['mahasiswa_nama']) . '</b>, ';
+                $dataString .= 'sudah melakukan  <b>' . htmlspecialchars($item['tracking']) . '</b>';
+                // Add other fields as needed
+                session()->setFlashdata('alert_message_' . htmlspecialchars($item['mahasiswa_nama']), $dataString);
+            }
+        }
+
+        $operation['title'] = 'Home';
+        $operation['sub_title'] = 'Sistem Informasi Manajemen Tugas Akhir';
+
+        return view("dashboard/index", $operation);
+    }
 }
